19e31c115ee60ac1f85b834fb91bd71fdc326e5c
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-08-26 09:40:21 -0400

    Mensajes de conectado y deconectado

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,2 +1,16 @@
+//Reference HTML
+const connect = document.querySelector('#connected');
+const disconnect = document.querySelector('#disconnected');
 
 const socket = io();
+
+socket.on('connect', () => {
+    console.log('Connected to server');
+    connect.style.display = '';
+    disconnect.style.display = 'none';
+});
+socket.on('disconnect', () => {
+    console.log('Disconnected from server');
+    connect.style.display = 'none';
+    disconnect.style.display = '';
+});
